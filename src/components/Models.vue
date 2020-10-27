011cceedbe9591f587ca93f05e1e0344ceaf840d
Hongyi Tao <hongyi.tao@uqconnect.edu.au>
2020-10-27 23:43:02 +1000

    model

--- src/components/Models.vue
+++ src/components/Models.vue
@@ -11,20 +11,20 @@
         <div v-else-if="model_title === 'Building Blocks of Life'">
             <div class="sketchfab-embed-wrapper">
                 <iframe title="A 3D model" width="640" height="480" src="https://sketchfab.com/models/6ef179b56e1a4d5493b58e1d883e27a4/embed?autostart=0&amp;ui_controls=1&amp;ui_infos=1&amp;ui_inspector=1&amp;ui_stop=1&amp;ui_watermark=1&amp;ui_watermark_link=1" frameborder="0" allow="autoplay; fullscreen; vr" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
-                <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;">
+                <!-- <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;">
                     <a href="https://sketchfab.com/3d-models/building-blocks-of-life-6ef179b56e1a4d5493b58e1d883e27a4?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank" style="font-weight: bold; color: #1CAAD9;">Building Blocks of Life</a>
                     by <a href="https://sketchfab.com/tobythy?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank" style="font-weight: bold; color: #1CAAD9;">tobythy</a>
                     on <a href="https://sketchfab.com?utm_medium=embed&utm_source=website&utm_campaign=share-popup" target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a>
-                </p>
+                </p> -->
             </div>
 
         </div>
 
         <div v-else-if="model_title === 'Apparatus for Germination of Achaemienis Ambulatii (Pony Plant)'">
             <vue-three-sixty 
-                :amount=14
-                imagePath="https://raw.githubusercontent.com/tobythy/botanic-garden-brisbane/main/models/Apparatus/"
-                fileName="Layer%20{index}.jpg"
+                :amount=29
+                imagePath="https://raw.githubusercontent.com/tobythy/botanic-garden-brisbane/main/models/plant2/"
+                fileName="{index}.jpg"
             />
         </div>
 
@@ -62,16 +62,11 @@
         </div>
 
         <div v-else-if="model_title === 'Jemmy Morrill and the brolgas'">
-            <div class="sketchfab-embed-wrapper">
-                <iframe
-                    title="A 3D model"
-                    src="https://sketchfab.com/models/eca4fdaddd504f2da24c0863c0c2333c/embed?autostart=0&amp;ui_controls=1&amp;ui_infos=1&amp;ui_inspector=1&amp;ui_stop=1&amp;ui_watermark=1&amp;ui_watermark_link=1"
-                    frameborder="0"
-                    allow="autoplay; fullscreen; vr"
-                    mozallowfullscreen="true"
-                    webkitallowfullscreen="true"
-                ></iframe>
-            </div>
+            <vue-three-sixty 
+                :amount=29
+                imagePath="https://raw.githubusercontent.com/tobythy/botanic-garden-brisbane/main/models/jemmy/"
+                fileName="{index}.jpg"
+            />
         </div>
 
         <div v-else-if="model_title === 'Big Red (Red Kangaroo)'">
@@ -90,7 +85,7 @@
         </div>
 
         <div v-else>
-            <p>model not exist</p>
+            <p>{{ model_title }}model not exist</p>
         </div>
 
     </div>
@@ -133,9 +128,9 @@ export default {
         min-height: 100%;
     }
 
-    .v360-viewer-container {
+    /* .v360-viewer-container {
         height: 100%;
-    }
+    } */
     .v360-fullscreen-toggle {
         position: absolute;
         top: 30px;
